208c5cb7d61e1d55a8e810d5c197105e11c464ac
Scott Schultz <dev@jotunga.com>
2013-06-09 23:05:44 +1000

    Continued splitting functions, added a few more stat triggers

---
+++ cfg/botGlobals.js
@@ -0,0 +1,32 @@
+var Steam		=	require('steam');
+var mysql		=	require('mysql');
+var fs			=	require('fs');
+var cfg			=	require('./config');
+var bot			=	new Steam.SteamClient();
+var stdin		=	process.openStdin();
+
+bot.steamChatRoomID = '103582791429601458'; // remember to pass this into the bots config at some point
+
+var colours = {
+	reset : "\033[0m",
+	red : "\033[31m",
+	green : "\033[32m",
+	yellow : "\033[33m",
+	blue : "\033[34m",
+	magenta : "\033[35m",
+	cyan : "\033[36m",
+	white : "\033[37m",
+}
+
+exports.Steam = Steam;
+exports.mysql = mysql;
+exports.fs = fs;
+exports.cfg = cfg;
+exports.stdin = stdin;
+exports.colour = colours;
+exports.bot = bot;
+
+exports.loadModule = function(module){
+    delete require.cache[require.resolve(module)]
+    return require(module)
+}
--- steam-bot.js
+++ steam-bot.js
@@ -1,10 +1,5 @@
-var Steam		=	require('steam');
-var mysql		=	require('mysql');
-var fs			=	require('fs');
-var cfg			=	require('./cfg/config');
-var commandFile;
-var bot		=	new Steam.SteamClient();
-var stdin	=	process.openStdin();
+var june = require('./cfg/botGlobals');
+var bot = june.bot;
 
 console.log("");
 console.log("+-----------------------------------------------------------------------------+");
@@ -16,42 +11,26 @@ console.log("|                     A mid-year project by Scott Schultz          
 console.log("|                                                                             |");
 console.log("+-----------------------------------------------------------------------------+");
 
-function requireUncached(module){
-    delete require.cache[require.resolve(module)]
-    return require(module)
-}
-
-var steamChatRoomID = '103582791429601458';
 // If the bot is given -passive as its startup argument, it will not log data, but it will still output it to the cmd window
-
-var reset = "\033[0m";
-var red = "\033[31m";
-var green = "\033[32m";
-var yellow = "\033[33m";
-var blue = "\033[34m";
-var magenta = "\033[35m";
-var cyan = "\033[36m";
-var white = "\033[37m";
-
 if(process.argv[2]!="-passive"){
-	var connection = mysql.createConnection(cfg.database);
+	june.connection = june.mysql.createConnection(june.cfg.dbInfo);
 
-	connection.connect(function(err) {
+	june.connection.connect(function(err) {
 		if(err) {
-			console.log(red+"[June]"+reset+" Connection failed! "+err);
+			console.log(june.colour.red+"[June]"+june.colour.reset+" Connection failed! "+err);
 			process.exit();
 		} else {
-			console.log(green+"[June]"+reset+" Connected and ready to go.");
+			console.log(june.colour.green+"[June]"+june.colour.reset+" Connected and ready to go.");
 		}
 	});
 	passiveMode = false;
 } else {
 	passiveMode = true;
 }
-bot.logOn(cfg.steam.username,cfg.steam.password);
+
+bot.logOn(june.cfg.steamAccount.username,june.cfg.steamAccount.password);
 botBirth = new Date();
 bot.on('loggedOn', function() {
-
 	bot.botStats = {
 		botBirth : botBirth.getTime()/1000,
 		messagesLogged : 0,
@@ -60,119 +39,38 @@ bot.on('loggedOn', function() {
 		validCommandRequests : 0,
 		invalidCommandRequests : 0,
 		emptyQuotes : 0,
-		selfEmptyQuotes : 0
+		selfEmptyQuotes : 0,
+		gamesPlayed: 0
 	}
-	console.log(green+'[June]'+reset+' Bot Logged in');
+	console.log(june.colour.green+'[June]'+june.colour.reset+' Bot Logged in');
 	if(passiveMode){
-		console.log(green+'[June]'+reset+' Not logging chat');
+		console.log(june.colour.green+'[June]'+june.colour.reset+' Not logging chat');
 	} else {
-		console.log(green+'[June]'+reset+' Logging chat');
-		bot.joinChat(steamChatRoomID);
+		console.log(june.colour.green+'[June]'+june.colour.reset+' Logging chat');
 	}
-	bot.setPersonaState(Steam.EPersonaState.Online); // to display your bot's status as "Online"
+	bot.joinChat(bot.steamChatRoomID);
+	bot.setPersonaState(june.Steam.EPersonaState.Online); // to display your bot's status as "Online"
 
-	stdin.addListener("data", function(d) {
-		botMessage = d.toString();
-		bot.sendMessage(steamChatRoomID,botMessage);
-		listener = requireUncached('./lib/chatListener');
-		listener.listenToChat(botMessage,bot.steamID,'grpmsg',bot,connection,steamChatRoomID);
-
-		// geturl = new RegExp("(^|[ \t\r\n])((ftp|http|https|gopher|mailto|news|nntp|telnet|wais|file|prospero|aim|webcal):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))","g");
-		// linkCount = 0;
-		// urlList = botMessage.match(geturl);
-		// if(urlList){
-		// 	linkCount = urlList.length;
-		// 	console.log(green+"[RESPONSE]"+reset+" That message had one or more links.");
-		// 	console.log(cyan+"[Matches] "+reset+" "+linkCount);
-		// 	for (var i = 0; i < linkCount; i++) {
-		// 		insertURL = urlList[i];
-		// 		connection.query("INSERT INTO `links` (`timestamp`,`steamID`,`linkURL`) VALUES (NOW(),"+chatter+","+connection.escape(insertURL)+")", function(err, rows, fields) {
-		// 			if(!err) {
-		// 				console.log(green+"[LOGGED]"+reset+" Found and logged a link from: "+chatter);
-		// 			} else {
-		// 				throw err
-		// 			}
-		// 		});
-		// 	}
-		// }
-		// wordCount = botMessage.match(/\S+/gi).length;
-		// wordCount = wordCount-linkCount;
-		// connection.query("INSERT INTO `logs` (`timestamp`,`steamID`,`chatMessage`,`wordCount`,`linkCount`,`emptyQuoteCount`,`type`) VALUES (NOW(),"+bot.steamID+","+connection.escape(bot.users[bot.steamID].playerName)+","+connection.escape(botMessage)+","+wordCount+","+linkCount+",'grpmsg')", function(err, rows, fields) {
-		// 	if(!err) {
-		// 		console.log(green+"[LOGGED]"+reset+" "+bot.users[bot.steamID].playerName+' ('+bot.steamID+'): '+botMessage);
-		// 	} else {
-		// 		throw err
-		// 	}
-		// });
+	june.stdin.addListener("data", function(d) {
+		june.loadModule('../lib/botSTDIN').listenConsole(d,june);
 	});
-
 });
 bot.on('friendMsg', function(source, message, type) { // friend messages
-	commandFile = requireUncached('./lib/chatCommands');
-	if(!passiveMode) {
-		if(message){
-			commandFile.getActiveCommand(message,source,bot,connection);
-		}
-	} else {
-		if(message){
-			commandFile.getPassiveCommand(message,source,bot,connection);
-		}
-	}
+	commandFile = june.loadModule('../lib/chatCommands').getActiveCommand(message,source,june);
 });
 bot.on('chatMsg', function(chatRoom, message, type, chatter) { // chat room messages
-	listener = requireUncached('./lib/chatListener');
-	if(!passiveMode) {
-		if(message){
-			listener.listenToChat(message,chatter,type,bot,connection,chatRoom);
-		}
-	} else {
-		console.log(yellow+"[OBSERVED] "+reset+" "+bot.users[chatter].playerName+' ('+chatter+'): '+message);
-	}
+	listener = june.loadModule('../lib/chatListener').listenToChat(message,chatter,type,chatRoom,june);
 });
 bot.on('chatStateChange',function(state,victim,sourceRoom,executor){
-	if(!passiveMode) {
-		connection.query("INSERT INTO `kickbans` (`timestamp`,`victimID`,`executorID`,`kickbanType`) VALUES (NOW(),"+victim+",'"+executor+"','"+state+"')", function(err, rows, fields) {
-			if(!err) {
-				console.log(green+"[LOGGED]"+reset+" "+victim+" ("+bot.users[victim].playerName+") chat state altered by "+executor+" ("+bot.users[executor].playerName+") method > "+state);
-			} else {
-				throw err
-			}
-		});
-	} else {
-		console.log(yellow+"[OBSERVED] "+reset+" "+victim+" ("+bot.users[victim].playerName+") chat state altered by "+executor+" ("+bot.users[executor].playerName+") method > "+state);
-	}
-	if(victim==bot.steamID) {
-		console.log("[What a bitch] "+executor+" just kicked me!");
-		if(state=='8'){
-			randomJoin = Math.floor(Math.random()*500);
-			setTimeout(bot.joinChat(steamChatRoomID),randomJoin);
-			setTimeout(bot.sendMessage(steamChatRoomID,"That was mean :("),randomJoin);
-		}
-	} else if(victim=='76561197994626023') {
-		if(state=='8'){
-			console.log(magenta+"[lol] bye brae."+reset);
-			braesKicked++;
-		}
-	}
-	//  1 : joined
-	//  2 : left chat
-	//  3 :
-	//  4 : disconnected
-	//  5 :
-	//  6 :
-	//  7 :
-	//  8 : kicked
-	//  9 : 
-	// 10 :
-	// 11 :
-	// 12 :
-	// 13 :
-	// 14 :
-	// 15 :
-	// 16 : banned
+	listener = june.loadModule('../lib/roomWatcher').observeChat(state,victim,sourceRoom,executor,june);
+});
+bot.on('personaState',function(personaObject){
+	listener = june.loadModule('../lib/personaWatcher').observeChat(personaObject,june);
 })
 bot.on('chatInvite', function(chatRoomID, chatRoomName, patronID) {
-	console.log('Got an invite to ' + chatRoomName +'('+chatRoomID+') from ' + bot.users[patronID].playerName);
+	console.log('Got an invite to '+chatRoomName+'('+chatRoomID+') from '+patronID);
 	bot.joinChat(chatRoomID); // autojoin on invite
 });
-	
+process.on('error',function(){
+	console.log(june.colour.red+"[ERROR]"+june.colour.reset+" shit fucked up somewhere.");
+})
